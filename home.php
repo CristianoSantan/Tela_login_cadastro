b483a5ed4056ccb1e0626f14d133bd635aa9fe31
CristianoSantan <cristiano.mer@gmail.com>
2020-10-29 17:46:09 -0300

    bootstrap

--- home.php
+++ home.php
@@ -14,11 +14,41 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
     <title>home</title>
 </head>
 <body>
-    
-    <h1>Olá, <?php echo $_SESSION['nome']; ?></h1>
 
+    <header class="container-fluid border shadow">
+        <nav class="row container m-auto">
+            <div class="col-10 d-flex align-items-center">
+                <img class="rounded-circle" src="<?php echo $_SESSION['imagem'] ?>" alt="<?php echo $_SESSION['nome'] ?>">
+                <h5 class="ml-3 mb-0"><?php echo $_SESSION['nome'] ?></h5>
+            </div>
+            <div class="col-2 d-flex align-items-center justify-content-end">
+                <div class="dropdown">
+                    <button class="btn text-white rounded-circle dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle= >
+
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#" >Meu perfil</a>
+                        <a class="dropdown-item" href="sair.php">Sair</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main class="container">
+        <form action="" class="form-row">
+            <input type="text" class="col-9 form-control" placeholder="No que você está pensando, <?php echo $_SESSION['nome'] ?>?">
+            <button class="btn">Publicar</button>
+        </form>
+    </main>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 </body>
 </html>
